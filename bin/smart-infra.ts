dbbee74b6bb7537e9f080c4ab98c67e8cb2e6cc0
Liwei <uccblw@gmail.com>
2020-10-11 23:33:28 +0900

    apigateway lambda dynamodb -- serverless

--- bin/smart-infra.ts
+++ bin/smart-infra.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { SmartInfraStack } from '../lib/smart-infra-stack';
 
 const app = new cdk.App();
-new SmartInfraStack(app, 'SmartInfraStack');
+new SmartInfraStack(app, 'SmartInfraStack',{ 
+    env: {
+        account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION 
+    }
+});
--- lib/smart-infra-stack.ts
+++ lib/smart-infra-stack.ts
@@ -1,9 +1,53 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as api from '@aws-cdk/aws-apigateway';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import {ApiGatewayToLambda,ApiGatewayToLambdaProps} from '@aws-solutions-constructs/aws-apigateway-lambda';
+import { LambdaToDynamoDB, LambdaToDynamoDBProps } from '@aws-solutions-constructs/aws-lambda-dynamodb';
 
 export class SmartInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // hello function responding to http requests 
+    const helloFunc = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
+
+    // hit counter, aws-lambda-dynamodb pattern
+    const lambda_ddb_props: LambdaToDynamoDBProps = {
+      lambdaFunctionProps: {
+          code: lambda.Code.asset(`lambda`),
+          runtime: lambda.Runtime.NODEJS_12_X,
+          handler: 'hitcounter.handler',
+          environment: {
+              DOWNSTREAM_FUNCTION_NAME: helloFunc.functionName
+          }
+      },
+      dynamoTableProps: {
+          tableName: 'Hits',
+          partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }
+      }
+    };
+
+    const hitcounter = new LambdaToDynamoDB(this, 'LambdaToDynamoDB', lambda_ddb_props);
+
+    // grant the hitcounter lambda role invoke permissions to the hello function
+    helloFunc.grantInvoke(hitcounter.lambdaFunction);
+
+    const api_lambda_props: ApiGatewayToLambdaProps = {
+      existingLambdaObj: hitcounter.lambdaFunction,
+      apiGatewayProps: {
+        defaultMethodOptions: {
+          authorizationType: api.AuthorizationType.NONE
+        }
+      }
+    };
+
+    new ApiGatewayToLambda(this,'ApiGatewayToLambda', api_lambda_props);
   }
 }
